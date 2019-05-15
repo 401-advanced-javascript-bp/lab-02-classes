94c1f976936088f3ccf16b33751cbf0142169e47
rebecca-pete <rebecca@rebeccapeterson.us>
2019-05-14 19:36:24 -0700

    update vehicle files and start to work on list

--- objects/vehicle-factory.js
+++ objects/vehicle-factory.js
@@ -1,7 +1,39 @@
-
 'use strict';
 
-function CarFactory() {
+const Car = () => ({
+  drive: () => {
+    console.log('Driving...');
+  },
+  stop: () => {
+    console.log('Stopping');
+  },
+});
+
+function CarFactory(name) {
+  let car = Object.assign(
+    {},
+    {name},
+    {wheels: 4},
+    Car()
+  );
+
+  // function vroom () {console.log('Loud exhaust sounds.')};
+
+  return Object.freeze(car);
 }
 
-module.exports = CarFactory;
+function MotorcycleFactory(name) {
+  let motorcycle = Object.assign(
+    {},
+    {name},
+    {wheels: 2},
+    {wheelie},
+    Car()
+  );
+
+  function wheelie () {console.log('Wheee!')};
+
+  return Object.freeze(motorcycle);
+}
+
+module.exports = {CarFactory, MotorcycleFactory};
